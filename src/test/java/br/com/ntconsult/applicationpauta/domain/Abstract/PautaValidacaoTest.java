78781b95a7c0c1035701d1f5e54bd58e17dffa36
Igor Queiroz <igorqueirozz@outlook.com>
2021-06-09 18:09:40 -0300

    #Revisão do código

--- src/test/java/br/com/ntconsult/applicationpauta/domain/Abstract/PautaValidacaoTest.java
+++ src/test/java/br/com/ntconsult/applicationpauta/domain/Abstract/PautaValidacaoTest.java
@@ -10,14 +10,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PautaValidacaoTest {
 
     @Autowired
